13190960e5691c8f9fd06c6d6bf76ac83efd7551
Andrea Bioddo <andrea.bioddo@edu.unito.it>
2023-01-11 14:53:51 +0100

    Services added

--- src/app/global.service.ts
+++ src/app/global.service.ts
@@ -1,25 +1,17 @@
 import { Injectable } from "@angular/core";
-
-
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import {map} from "rxjs/operators";
-import { User } from "./user";
+import { User } from "./model/user";
+
 @Injectable()
 export class GlobalService{
-
-
     private baseURL = "http://localhost:3000";
-    
-
-    //List of service that use REST API to get the values and each function will return an observable
-    // doLogin():Observable<User>{
-    //     // return this.http.get<User>(this.baseURL + "/login");  
-    //     alert('me');      
-    // }
-    doLogin(){
-        // return this.http.get<User>(this.baseURL + "/login");  
-        alert('me');      
+    constructor(private http: HttpClient){
     }
 
+    //List of service that use REST API to get the values and each function will return an observable
+    doLogin(name:string, password:string):Observable<User>{
+        return this.http.post<User>(this.baseURL + "/login", {name:name, password:password});        
+    }
 }
